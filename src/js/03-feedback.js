5c0edbf4938f46857043111104cdab338a25685c
VitaliiKablukov <vetal.kablukov98@gmail.com>
2022-08-19 01:09:50 +0300

    task3-complete

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+//
+if (localStorage.getItem(LOCALSTORAGE_KEY)) {
+  const parseLocalStograde = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  form.elements.email.value = parseLocalStograde.email;
+  form.elements.message.value = parseLocalStograde.message;
+}
+function saveDataForm(e) {
+  //
+  e.preventDefault();
+  //
+  const formInputs = {};
+  formInputs.email = form.elements.email.value;
+  formInputs.message = form.elements.message.value;
 
+  //
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formInputs));
+}
+
+form.addEventListener('input', throttle(saveDataForm, 500));
+//
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  console.log(localStorage.getItem(LOCALSTORAGE_KEY));
+  form.reset();
+  localStorage.clear();
+});
